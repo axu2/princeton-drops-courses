71b4b4591ff75eff9111bcfcc2fcf72a67ef70bd
ax2 <alexkurosakimh3@gmail.com>
2019-02-03 12:46:29 -0500

    remove print statements

--- models.py
+++ models.py
@@ -25,7 +25,6 @@ class Course(Document):
         ]
     }
 
-i = 0
 def main(term="1194"):
     url = "https://registrar.princeton.edu/course-offerings/search_results.xml?submit=Search&term=" + term
     html = requests.get(url).text
@@ -49,8 +48,6 @@ def main(term="1194"):
             course.max_enroll.append(cols[9][0])
 
         course.save()
-        print(i)
-        i += 1
 
 if __name__ == '__main__':
     main()
